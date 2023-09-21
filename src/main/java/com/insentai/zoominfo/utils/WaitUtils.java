5a616b1758469a08783eebfb05ee4c8b1032f816
KumareshVaradharajan <94116761+KumareshVaradharajan@users.noreply.github.com>
2023-09-21 11:00:40 +0530

    Added WaitUtils utility class for handling explicit waits

---
+++ src/main/java/com/insentai/zoominfo/utils/WaitUtils.java
@@ -0,0 +1,36 @@
+package com.insentai.zoominfo.utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitUtils {
+
+    private WebDriver driver;
+
+    public WaitUtils(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Reusable method for waiting for an element to be clickable
+    public WebElement waitForElementToBeClickable(By locator, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    // Reusable method for waiting for an element to be visible
+    public WebElement waitForElementToBeVisible(By locator, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    // Reusable method for waiting for an element to be present in the DOM
+    public WebElement waitForElementToBePresent(By locator, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+}
